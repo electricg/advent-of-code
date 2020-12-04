79920470d2cfd9a2a9658cbcd5c79b64c627737b
Giulia Alfonsi <electric.g@gmail.com>
2020-12-03 21:36:51 -0800

    Year 2020 day 04 part 01

--- 2020/04/1.js
+++ 2020/04/1.js
@@ -2,4 +2,28 @@ import Helpers from '../helpers.js';
 
 const fileName = './input.txt';
 
-const input = Helpers.readFileLines(fileName, import.meta.url);
+const input = Helpers.readFile(fileName, import.meta.url);
+
+const mandatoryFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
+const len = mandatoryFields.length;
+
+const parsePassport = input => input.trim().split(/\s/).reduce((acc, item) => {
+    const [key, value] = item.split(':');
+    acc[key] = value;
+    return acc;
+}, {});
+
+const isValid = passport => {
+    for (let i = 0; i < len; i++) {
+        if(!passport[mandatoryFields[i]]) {
+            return false;
+        }
+    }
+    return true;
+};
+
+const parseBatch = input => input.split('\n\n').map(parsePassport);
+
+const validatePassports = parseBatch(input).filter(isValid);
+
+console.log(validatePassports.length);
