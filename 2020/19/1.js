6495c0aa9fde488d9821ae18770ae4d26ef05333
Giulia Alfonsi <electric.g@gmail.com>
2020-12-20 23:35:05 -0800

    Year 2020 day 19 part 1

---
+++ 2020/19/1.js
@@ -0,0 +1,69 @@
+import Helpers from '../helpers.js';
+
+const fileName = './input.txt';
+
+const input = Helpers.readFile(fileName, import.meta.url);
+
+const parseInput = input => {
+    const [rules, messages] = input.trim().split('\n\n');
+    const parsedRules = rules.split('\n').reduce((acc, line) => {
+        let [key, rule] = line.split(/\s*:\s*/);
+        if (rule.indexOf('"') !== -1) {
+            rule = rule.replace(/"/g, '');
+        }
+        acc[key] = rule;
+        return acc;
+    }, {});
+    return {
+        rules: parsedRules,
+        messages: messages.split('\n')
+    };
+};
+
+const createRegExp = (string, data) => {
+    while(/\d/.test(string)) {
+        string = string.replace(/\d+/g, match => `(${data[match]})`);
+    }
+    return new RegExp(`^${string.replace(/ /g, '')}$`);
+};
+
+const calcSolution = input => {
+    const parsedInput = parseInput(input);
+    // console.log(parsedInput);
+    const { rules, messages } = parsedInput;
+    const x = createRegExp(rules[0], rules);
+    return messages.filter(m => x.test(m)).length;
+};
+
+const tests = [
+    {
+        inp: `
+0: 4 1 5
+1: 2 3 | 3 2
+2: 4 4 | 5 5
+3: 4 5 | 5 4
+4: "a"
+5: "b"
+6: 4 5
+
+ababbb
+bababa
+abbbab
+aaabbb
+aaaabbb
+`,
+        out: 2
+    }
+];
+
+tests.forEach(({ inp, out }) => {
+    const res = calcSolution(inp);
+    if (res === out) {
+        console.log(`✅`);
+    }
+    else {
+        console.error(`❌`);
+    }
+});
+
+console.log(calcSolution(input));
