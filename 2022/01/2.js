d63a6ceca0f1a9116d573e59f7c0c857e0907c27
Giulia Alfonsi <electric.g@gmail.com>
2023-02-23 11:02:48 -0800

    Year 2022 day 01 part 2

---
+++ 2022/01/2.js
@@ -0,0 +1,51 @@
+import Helpers from '../helpers.js';
+
+const fileName = './input.txt';
+
+const input = Helpers.readFile(fileName, import.meta.url);
+
+const parseInput = (input) =>
+  input
+    .trim()
+    .split('\n\n')
+    .map((elf) => elf.split('\n').map((cal) => cal * 1));
+
+const calcSolution = (input) => {
+  const parsedInput = parseInput(input);
+  // console.log(parsedInput);
+  const arr = parsedInput.map((elf) => Helpers.sumArray(elf));
+  return Helpers.sumArray(arr.sort(Helpers.compareNumbersDesc).slice(0, 3));
+};
+
+const tests = [
+  {
+    inp: `
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+`,
+    out: 45000,
+  },
+];
+
+tests.forEach(({ inp, out }) => {
+  const res = calcSolution(inp);
+  if (res === out) {
+    console.log(`✅`);
+  } else {
+    console.error(`❌`);
+  }
+});
+
+console.log(calcSolution(input));
