ced165b014bed900d6d6527204d882fbe077ced3
Giulia Alfonsi <electric.g@gmail.com>
2024-07-02 17:07:12 -0700

    Year 2022 day 09 part 1

--- 2022/09/1.js
+++ 2022/09/1.js
@@ -13,10 +13,80 @@ const parseInput = (input) =>
       return [dir, n * 1];
     });
 
+const moves = {
+  R: {
+    x: 1,
+    y: 0,
+  },
+  L: {
+    x: -1,
+    y: 0,
+  },
+  U: {
+    x: 0,
+    y: 1,
+  },
+  D: {
+    x: 0,
+    y: -1,
+  },
+};
+
 const calcSolution = (input) => {
   const parsedInput = parseInput(input);
-  console.log(parsedInput);
-  return parsedInput;
+  // console.log(parsedInput);
+
+  const H = { x: 0, y: 0 };
+  const T = { x: 0, y: 0 };
+
+  const visitedByT = {};
+
+  parsedInput.forEach(([dir, n]) => {
+    // console.log(dir, n);
+    for (let i = 0; i < n; i++) {
+      H.x += moves[dir].x;
+      H.y += moves[dir].y;
+
+      const distX = H.x - T.x;
+      const distY = H.y - T.y;
+
+      let isDiagonal = false;
+
+      if (Math.abs(distX * distY) === 2) {
+        // diagonal
+        isDiagonal = true;
+      }
+
+      if (distX > 1) {
+        T.x += 1;
+      } else if (distX < -1) {
+        T.x += -1;
+      }
+
+      if (distY > 1) {
+        T.y += 1;
+      } else if (distY < -1) {
+        T.y += -1;
+      }
+
+      if (isDiagonal) {
+        if (distX === 1 || distX === -1) {
+          T.x += distX;
+        }
+        if (distY === 1 || distY === -1) {
+          T.y += distY;
+        }
+      }
+
+      visitedByT[`${T.x}_${T.y}`] = 1;
+
+      // console.log(dir, i, H, T, distX, distY);
+    }
+    // console.log('===');
+  });
+  // console.log(visitedByT);
+
+  return Object.keys(visitedByT).length;
 };
 
 const tests = [
@@ -44,4 +114,4 @@ tests.forEach(({ inp, out }) => {
   }
 });
 
-// console.log(calcSolution(input));
+console.log(calcSolution(input));
