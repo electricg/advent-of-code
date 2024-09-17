fb274794d63113959273050ed895776051f539e9
Giulia Alfonsi <electric.g@gmail.com>
2024-09-17 15:58:50 -0700

    Year 2022 day 13 part 2

---
+++ 2022/13/2.js
@@ -0,0 +1,93 @@
+import Helpers from '../helpers.js';
+
+const fileName = './input.txt';
+
+const input = Helpers.readFile(fileName, import.meta.url);
+
+const parseInput = (input) =>
+	input
+		.trim()
+		.split('\n')
+		.filter((line) => line !== '')
+		.map(JSON.parse);
+
+// Return -1 right order, 1 wrong order, 0 continue checking
+const compare = (a, b) => {
+	if (typeof a === 'number' && typeof b === 'number') {
+		return a - b;
+	}
+
+	if (typeof a === 'number') {
+		a = [a];
+	} else if (typeof b === 'number') {
+		b = [b];
+	}
+
+	for (let i = 0; i < a.length; i++) {
+		if (b[i] === undefined) {
+			return 1;
+		}
+
+		const c = compare(a[i], b[i]);
+		if (c !== 0) {
+			return c;
+		}
+	}
+
+	return a.length === b.length ? 0 : -1;
+};
+
+const calcSolution = (input) => {
+	const parsedInput = parseInput(input);
+	// console.log(parsedInput);
+
+	const divs = [[[2]], [[6]]];
+	const list = parsedInput.concat(divs).sort(compare);
+	// console.log(list);
+
+	const result = divs.reduce((acc, div) => acc * (list.indexOf(div) + 1), 1);
+	// console.log(result);
+	return result;
+};
+
+const tests = [
+	{
+		inp: `
+[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]
+`,
+		out: 140,
+	},
+];
+
+tests.forEach(({ inp, out }) => {
+	const res = calcSolution(inp);
+	if (res === out) {
+		console.log(`✅`);
+	} else {
+		console.error(`❌`);
+	}
+});
+
+console.log(calcSolution(input));
