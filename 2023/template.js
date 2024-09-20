7c465dce4b0b6c897a2184c34f7440b255fc72e0
Giulia Alfonsi <electric.g@gmail.com>
2024-09-20 09:22:11 -0700

    Improved 2023 template formatting

--- 2023/template.js
+++ 2023/template.js
@@ -5,29 +5,31 @@ const fileName = './input.txt';
 const input = Helpers.readFile(fileName, import.meta.url);
 
 const parseInput = (input) => {
-  return input;
+	return input;
 };
 
 const calcSolution = (input) => {
-  const parsedInput = parseInput(input);
-  console.log(parsedInput);
-  return parsedInput;
+	const parsedInput = parseInput(input);
+	console.log(parsedInput);
+	return parsedInput;
 };
 
 const tests = [
-  {
-    inp: '',
-    out: '',
-  },
+	{
+		inp: `
+
+`,
+		out: '',
+	},
 ];
 
 tests.forEach(({ inp, out }) => {
-  const res = calcSolution(inp);
-  if (res === out) {
-    console.log(`✅`);
-  } else {
-    console.error(`❌`);
-  }
+	const res = calcSolution(inp);
+	if (res === out) {
+		console.log(`✅`);
+	} else {
+		console.error(`❌`);
+	}
 });
 
 console.log(calcSolution(input));
