bcc256e5c7d204ebcc3d31d000d42c64397c39ca
Giulia Alfonsi <electric.g@gmail.com>
2024-09-25 12:36:02 -0700

    Year 2023 day 05 part 1

---
+++ 2023/05/1.js
@@ -0,0 +1,110 @@
+import Helpers from '../helpers.js';
+
+const fileName = './input.txt';
+
+const input = Helpers.readFile(fileName, import.meta.url);
+
+const parseInput = (input) => {
+	const rawMaps = input.trim().split('\n\n');
+
+	const seedsLine = rawMaps.shift();
+	const seeds = seedsLine
+		.trim()
+		.split(':')
+		.pop()
+		.trim()
+		.split(' ')
+		.map((item) => item * 1);
+
+	const maps = rawMaps.map((map) => {
+		const lines = map.trim().split('\n');
+		const id = lines.shift();
+
+		const instructions = lines.map((line) => line.split(' ').map((n) => n * 1));
+
+		return { id, instructions };
+	});
+
+	return {
+		seeds,
+		maps,
+	};
+};
+
+const calcSolution = (input) => {
+	const parsedInput = parseInput(input);
+	// console.log(parsedInput);
+
+	const { seeds, maps } = parsedInput;
+	const locations = seeds.map((seed) => {
+		let next = seed;
+
+		maps.forEach(({ instructions }) => {
+			for (let i = 0; i < instructions.length; i++) {
+				const instruction = instructions[i];
+				const [dest, source, len] = instruction;
+				if (next >= source && next < source + len) {
+					const diff = next - source;
+					next = dest + diff;
+					break;
+				}
+			}
+		});
+
+		return next;
+	});
+
+	return Math.min(...locations);
+};
+
+const tests = [
+	{
+		inp: `
+seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+`,
+		out: 35,
+	},
+];
+
+tests.forEach(({ inp, out }) => {
+	const res = calcSolution(inp);
+	if (res === out) {
+		console.log(`✅`);
+	} else {
+		console.error(`❌`);
+	}
+});
+
+console.log(calcSolution(input));
