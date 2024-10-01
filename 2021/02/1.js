c104e3dbd3bcaf2ce502cebf11de03c8809cc72b
Giulia Alfonsi <electric.g@gmail.com>
2024-10-01 12:02:20 -0700

    Year 2021 day 02 part 1

--- 2021/02/1.js
+++ 2021/02/1.js
@@ -4,17 +4,47 @@ const fileName = './input.txt';
 
 const input = Helpers.readFile(fileName, import.meta.url);
 
-const parseInput = input => {};
+const parseInput = input => {
+	return input
+		.trim()
+		.split('\n')
+		.map(line => {
+			const [dir, units] = line.split(' ');
+			return [dir, Number(units)];
+		});
+};
 
 const calcSolution = input => {
 	const parsedInput = parseInput(input);
-	console.log(parsedInput);
+	// console.log(parsedInput);
+
+	let horizontal = 0;
+	let depth = 0;
+
+	parsedInput.forEach(([dir, units]) => {
+		if (dir === 'forward') {
+			horizontal += units;
+		} else if (dir === 'down') {
+			depth += units;
+		} else if (dir === 'up') {
+			depth -= units;
+		}
+	});
+
+	return horizontal * depth;
 };
 
 const tests = [
 	{
-		inp: '',
-		out: '',
+		inp: `
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+`,
+		out: 150,
 	},
 ];
 
