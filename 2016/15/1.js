e4d514fc35e83eb80fe893c90a01e39bee9b89a4
Giulia Alfonsi <electric.g@gmail.com>
2024-10-03 10:35:27 -0700

    Year 2016 day 15 part 1 NOT even started

---
+++ 2016/15/1.js
@@ -0,0 +1,33 @@
+import Helpers from '../helpers.js';
+
+const fileName = './input.txt';
+
+const input = Helpers.readFile(fileName, import.meta.url);
+
+const parseInput = (input) => input.trim();
+
+const calcSolution = (input) => {
+  const parsedInput = parseInput(input);
+  console.log(parsedInput);
+  return 0;
+};
+
+const tests = [
+  {
+    inp: `
+
+`,
+    out: '',
+  },
+];
+
+tests.forEach(({ inp, out }) => {
+  const res = calcSolution(inp);
+  if (res === out) {
+    console.log(`✅`);
+  } else {
+    console.error(`❌`);
+  }
+});
+
+// console.log(calcSolution(input));
