5247615f690f7e4a20b40ebd70c274c9f8bff5e6
Giulia Alfonsi <electric.g@gmail.com>
2024-10-07 23:33:49 -0700

    Year 2016 day 25 part 1

--- 2016/25/1.js
+++ 2016/25/1.js
@@ -4,29 +4,86 @@ const fileName = './input.txt';
 
 const input = Helpers.readFile(fileName, import.meta.url);
 
-const parseInput = (input) => input.trim();
+const parseInput = (input) =>
+  input
+    .trim()
+    .split('\n')
+    .map((line) => line.split(' '));
 
-const calcSolution = (input) => {
-  const parsedInput = parseInput(input);
-  console.log(parsedInput);
+const checkOutput = (output) => {
+  if (!output.length) {
+    return false;
+  }
 
-  return 0;
+  if (output.join('').match(/[2-9]/)) {
+    return false;
+  }
+
+  for (let i = 1; i < output.length - 1; i++) {
+    if (output[i] === output[i - 1] || output[i] === output[i + 1]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
-const tests = [
-  {
-    inp: '',
-    out: '',
-  },
-];
+const calcSolution = (input, start) => {
+  const parsedInput = parseInput(input);
+  // console.log(parsedInput);
+  let current = 0;
+  let counter = 0;
 
-tests.forEach(({ inp, out }) => {
-  const res = calcSolution(inp);
-  if (res === out) {
-    console.log(`✅`);
-  } else {
-    console.error(`❌`);
+  const output = [];
+
+  const registers = {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 0,
+    ...start,
+  };
+  // console.log(registers);
+
+  while (counter < 100000) {
+    counter++;
+    const [what, x, y] = parsedInput[current];
+    current++;
+    // console.log(current, what, x, y);
+
+    if (what === 'cpy') {
+      const xN = isNaN(x) ? registers[x] : Number(x);
+      registers[y] = xN;
+    } else if (what === 'inc') {
+      registers[x]++;
+    } else if (what === 'dec') {
+      registers[x]--;
+    } else if (what === 'jnz') {
+      const xN = isNaN(x) ? registers[x] : Number(x);
+      const yN = isNaN(y) ? registers[y] : Number(y);
+      if (xN !== 0) {
+        current += yN - 1;
+      }
+    } else if (what === 'out') {
+      const xN = isNaN(x) ? registers[x] : Number(x);
+      output.push(xN);
+    }
   }
-});
 
-console.log(calcSolution(input));
+  if (checkOutput(output)) {
+    return true;
+  }
+
+  return false;
+};
+
+const calcSolutionFinal = (input) => {
+  for (let i = 0; i < 10000; i++) {
+    const res = calcSolution(input, { a: i });
+    if (res) {
+      return i;
+    }
+  }
+};
+
+console.log(calcSolutionFinal(input));
