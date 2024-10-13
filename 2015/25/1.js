c6b515f301397da8254ea9c7d3e857023152757d
Giulia Alfonsi <electric.g@gmail.com>
2024-10-13 16:42:28 -0700

    Year 2015 day 25 part 1

--- 2015/25/1.js
+++ 2015/25/1.js
@@ -5,31 +5,36 @@ const fileName = './input.txt';
 const input = Helpers.readFile(fileName, import.meta.url);
 
 const parseInput = (input) => {
-  return input;
+  const regex = /(\d+)[,a-zA-Z ]+(\d+)/g;
+  const matches = regex.exec(input);
+  return {
+    row: Number(matches[1]),
+    column: Number(matches[2]),
+  };
 };
 
 const calcSolution = (input) => {
   const parsedInput = parseInput(input);
-  console.log(parsedInput);
-  return parsedInput;
-};
+  // console.log(parsedInput);
 
-const tests = [
-  {
-    inp: `
+  const { row, column } = parsedInput;
 
-`,
-    out: '',
-  },
-];
+  let code = 20151125;
+  let x = 1;
+  let y = 1;
+  while (x !== row || y !== column) {
+    code = (code * 252533) % 33554393;
 
-tests.forEach(({ inp, out }) => {
-  const res = calcSolution(inp);
-  if (res === out) {
-    console.log(`✅`);
-  } else {
-    console.error(`❌`);
+    if (x === 1) {
+      x = y + 1;
+      y = 1;
+    } else {
+      y++;
+      x--;
+    }
   }
-});
+
+  return code;
+};
 
 console.log(calcSolution(input));
